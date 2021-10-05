4a9f0af71c9497c05317dece093bbb06791c1d7b
Jose Delfin Austria <analyst@secOps.localdomain>
2021-10-05 06:29:25 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="img/profilee.jpg" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">jose delfin austria</p>
+           <p class="regular">BSIT EVSU-OCC</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Ormoc City<br /> Leyte
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               9678498675
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               austriajd@gmail.com
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fab fa-weebly"></i>
+             </div>
+             <div class="data">
+               www.fb.com/austria
+             </div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skills</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               Compu- ter
+             </div>
+             <div class="skill_progress">
+               <span style="width: 75%;"></span>
+             </div>
+             <div class="skill_per">75%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Commu- nication
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Writing
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Competi- tiveness
+             </div>
+             <div class="skill_progress">
+               <span style="width: 85%;"></span>
+             </div>
+             <div class="skill_per">85%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Adap- tiveness
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_social">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fab fa-facebook-square"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Facebook</p>
+               <p>jdaustria@facebook</p>
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fab fa-twitter-square"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Twitter</p>
+               <p>jdaustria@twitter</p>
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fab fa-youtube"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Youtube</p>
+               <p>jdaustria@youtube</p>
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fab fa-linkedin"></i>
+             </div>
+             <div class="data">
+               <p class="semi-bold">Linkedin</p>
+               <p>jdaustria@linkedin</p>
+             </div>
+           </li>
+         </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About me</p>
+         </div>
+        <p>Born on June 14, 1999 in Quezon City, Manila. First child of Mirasol Sugarol and second child of Nadel Austria. I was a journalist in elementary to senior high and was
+		was able to compete to regionals. I passed the CCNA NC-II for Tesda. I am a proud member of LGBTQIA+ community.</p> 
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2019</div> 
+                <div class="info">
+                     <p class="semi-bold">Nestle Brand Ambassador</p> 
+                  <p>I was assigned as sampling assistant thrice. First was in Maasin, then in Robinson Centrum, and lastly to Robinson again.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Education</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2006 - 2008</div> 
+                <div class="info">
+                     <p class="semi-bold">St. Michael Montesorri Integrated School (Cainta, Rizal)</p> 
+                  <p>I studied in this private school for Grade 1 and Grade 2.</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2008 - 2009</div>
+              <div class="info">
+                     <p class="semi-bold">Camambugan Elementary School (Ubay, Bohol)</p> 
+                  <p>I pursued my Grade 3 in this public school.</p>
+                </div>
+            </li>
+			<li>
+              <div class="date">2009 - 2012</div>
+              <div class="info">
+                     <p class="semi-bold">Valencia Central School (Valencia, Ormoc)</p> 
+                  <p>I studied there for grade 4 to 6. I graduated with honors.</p>
+                </div>
+            </li>
+			<li>
+              <div class="date">2012 - 2016</div>
+              <div class="info">
+                     <p class="semi-bold">Valencia National High School (Valencia, Ormoc)</p> 
+                  <p>I graduated my Junior High here and was a consecutive honor student.</p>
+                </div>
+            </li>
+			<li>
+              <div class="date">2016 - 2018</div>
+              <div class="info">
+                     <p class="semi-bold">Western Leyte College, Inc. (Ormoc)</p> 
+                  <p>I graduated my Senior High here with the strand, TVL-ICT and was a consecutive honor student.</p>
+                </div>
+            </li>
+			<li>
+              <div class="date">2018 - present</div>
+              <div class="info">
+                     <p class="semi-bold">Eastern Visayas State University Ormoc Campus</p> 
+                  <p>I took up the course BSIT.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-music"></i></li>
+         <li><i class="fab fa-pagelines"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
